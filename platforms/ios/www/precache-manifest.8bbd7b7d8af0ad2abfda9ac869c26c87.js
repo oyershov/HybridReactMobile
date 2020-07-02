4196c06dc0b71b67a0081d2f5cb9d83885a8216b
oyershov <oyershov@heliostech.fr>
2020-07-02 18:53:29 +0300

    Basic project configuration

---
+++ platforms/ios/www/precache-manifest.8bbd7b7d8af0ad2abfda9ac869c26c87.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "15e4b73bbeaa8b8ea36e6c9396446519",
+    "url": "./index.html"
+  },
+  {
+    "revision": "c59bba948eb12ec65ed1",
+    "url": "./static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "0e38a54628643abbfd79",
+    "url": "./static/js/2.aca5da7f.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "./static/js/2.aca5da7f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c59bba948eb12ec65ed1",
+    "url": "./static/js/main.891f2303.chunk.js"
+  },
+  {
+    "revision": "a19d6302fdfd1549f5fc",
+    "url": "./static/js/runtime-main.760c15e9.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "./static/media/logo.5d5d9eef.svg"
+  }
+]);
